c6fb01d64f194405cf24d622dab61246ccd6ab3b
Peter Springer <peterspringer829@gmail.com>
2016-12-05 15:06:36 -0700

    add renderer.js

---
+++ renderer.js
@@ -0,0 +1,11 @@
+const {shell, remote} = require('electron');
+// const Vue = require('vue')
+// const Main = require('./main.js');
+const application = document.querySelector('.app')
+
+// new Vue({ // eslint-disable-line no-new
+//   el: '.app',
+//   render: (h) => h(Main)
+// })
+
+application.innerHTML = 'hellooo'
